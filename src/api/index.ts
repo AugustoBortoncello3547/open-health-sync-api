593d037fc5c07c8e9fea08c2464b00a816e2ebd0
AugustoBortoncello3547 <augusto.bortoncello@gmail.com>
2025-09-06 10:21:36 -0300

    fix: move created folder

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import { buildApp } from "../src";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { buildApp } from "..";
 
 export default async (req: FastifyRequest, res: FastifyReply) => {
   try {
