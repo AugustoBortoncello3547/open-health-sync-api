19598a1d5364f604e37f09af3c2de1a1c9a3cbf8
AugustoBortoncello3547 <augusto.bortoncello@gmail.com>
2025-09-07 19:35:43 -0300

    fix: import name

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
-import { app } from "../src/app";
+import { app } from "../src/app.js";
 import { type ZodTypeProvider } from "fastify-type-provider-zod";
 
 const fastify = Fastify({
